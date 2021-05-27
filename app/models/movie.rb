0464823f1065c977ca9570ef939a3a567b7685c2
Filipe La Feria <filipe.laferia@gmail.com>
2021-05-27 16:48:33 +0200

    Prepare rails app with external specs

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,8 @@
 class Movie < ApplicationRecord
+
+  has_many :bookmarks, dependent: :destroy
+
+  validates :title, :overview, :poster_url, :rating, presence: true
+  validates :title, uniqueness: true
+
 end
